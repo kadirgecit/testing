4b6c29aec7f4f56e62ad627e6c900cd169ad085d
Kadir Geçit <221954027@ogr.uludag.edu.tr>
2021-03-15 21:02:01 +0300

    Update install_new.py

--- install_new.py
+++ install_new.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import subprocess, os, random, string, sys, shutil, socket, shutil, time
+import subprocess, os, random, string, sys, shutil, socket, time
 from itertools import cycle, izip
 
 rDownloadURL = {"main": "https://www.dropbox.com/s/6zrfs5v0pxsnoy9/xtreamplus_base.tar.gz?dl=0", "sub": "0"}
@@ -20,31 +20,6 @@ class col:
     UNDERLINE = '\033[4m'
 
 def generate(length=16): return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(length))
-
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', autosize = False):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        autosize    - Optional  : automatically resize the length of the progress bar to the terminal window (Bool)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    styling = '%s |%s| %s%% %s' % (prefix, fill, percent, suffix)
-    if autosize:
-        cols, _ = shutil.get_terminal_size(fallback = (length, 1))
-        length = cols - len(styling)
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s' % styling.replace(fill, bar), end = '\r')
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
 
 def getIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -307,19 +282,25 @@ if __name__ == "__main__":
                     client = raw_input("  ")
                     printc("Enter Streaming port")
                     streaming = raw_input("  ")
-                    items = list(range(0, 57))
-                    l = len(items)
-                    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', autosize = True)
-                    for i, item in enumerate(items):
+                    toolbar_width = 40
+                    # setup toolbar
+                    sys.stdout.write("[%s]" % (" " * toolbar_width))
+                    sys.stdout.flush()
+                    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+                    for i in xrange(toolbar_width):
                         rRet = prepare(rType.upper())
                         if not install(rType.upper()): sys.exit(1)
                         if rType.upper() == "MAIN":
                             if not mysql(rUsername, rPassword): sys.exit(1)
                         encrypt(rHost, rUsername, rPassword, rDatabase, rServerID, rPort)
                         configure()
-                        time.sleep(0.1)
-                        # Update Progress Bar
-                        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', autosize = True)
+                        # update the bar
+                        sys.stdout.write("-")
+                        sys.stdout.flush()
+
+                    sys.stdout.write("]\n") # this ends the progress bar
+                    
                     os.system("clear")
                     print logo
                     print " "
